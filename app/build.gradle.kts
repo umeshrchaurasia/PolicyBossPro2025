07863027e5520f94c7b710116f91b91948f3be3f
Rahul Chaurasia <rahulrchaurasia@gmail.com>
2025-08-30 17:30:27 +0530

    common

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.policyboss.policybosspro"
         minSdk = 24
         targetSdk = 35
-        versionCode = 52
-        versionName = "1.5.0.2"
+        versionCode = 53
+        versionName = "1.5.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
